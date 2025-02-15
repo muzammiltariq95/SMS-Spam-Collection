cd0329cfc08379587598448d7dbe32ce17aeb917
MuzammilTariq <muzammiltariq95@gmail.com>
2025-02-15 01:19:34 +0000

    initial EDA

--- Yelp reviews.py
+++ Yelp reviews.py
@@ -1,6 +1,18 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set_style('white')
 
 yelp = pd.read_csv('yelp.csv')
 
 yelp.head()
+
+yelp.info()
+
+yelp.describe()
+
+yelp['text length'] = yelp['text'].apply(len)
+
+g = sns.FacetGrid(yelp,col='stars')
+g.map(plt.hist,'text length')
