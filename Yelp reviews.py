98d551fd9fc9cfd68d898130c97f1e2c3d194031
MuzammilTariq <muzammiltariq95@gmail.com>
2025-02-20 01:24:39 +0000

    completed code

--- Yelp reviews.py
+++ Yelp reviews.py
@@ -74,3 +74,8 @@ X_train, X_test, y_train, y_test = train_test_split(X, y,test_size=0.3,random_st
 
 # May take some time
 pipeline.fit(X_train,y_train)
+
+predictions = pipeline.predict(X_test)
+
+print(confusion_matrix(y_test,predictions))
+print(classification_report(y_test,predictions))
