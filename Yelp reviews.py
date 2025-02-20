17c34c9833e643c3d49e7451bfa4a5db127b4931
MuzammilTariq <muzammiltariq95@gmail.com>
2025-02-20 01:20:12 +0000

    updated code including count vectorizor and classification report

--- Yelp reviews.py
+++ Yelp reviews.py
@@ -17,3 +17,44 @@ yelp['text length'] = yelp['text'].apply(len)
 g = sns.FacetGrid(yelp,col='stars')
 g.map(plt.hist,'text length')
 plt.show()
+
+sns.boxplot(x='stars',y='text length',data=yelp,palette='rainbow')
+plt.show()
+
+sns.countplot(x='stars',data=yelp,palette='rainbow')
+plt.show()
+
+stars = yelp.groupby('stars').mean()
+stars
+
+stars.corr()
+
+sns.heatmap(stars.corr(),cmap='coolwarm',annot=True)
+plt.show()
+
+yelp_class = yelp[(yelp.stars==1) | (yelp.stars==5)]
+
+X = yelp_class['text']
+y = yelp_class['stars']
+
+from sklearn.feature_extraction.text import CountVectorizer
+cv = CountVectorizer()
+
+X = cv.fit_transform(X)
+
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(X, y,test_size=0.3,random_state=101)
+
+from sklearn.naive_bayes import MultinomialNB
+nb = MultinomialNB()
+
+nb.fit(X_train,y_train)
+
+predictions = nb.predict(X_test)
+
+from sklearn.metrics import confusion_matrix,classification_report
+
+print(confusion_matrix(y_test,predictions))
+print('\n')
+print(classification_report(y_test,predictions))
